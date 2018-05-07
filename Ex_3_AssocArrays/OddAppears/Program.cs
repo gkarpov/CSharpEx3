b702b89acfa34c5ef7d31736ee48c1704d2b2a3d
Georgiy Karpov <karpov21g@gmail.com>
2018-05-07 17:46:40 +0300

    Problems 1 and 2

---
+++ Ex_3_AssocArrays/OddAppears/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace OddAppears
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var strs = new Dictionary<string, int>();
+
+            List<string> input = Console.ReadLine().Split(' ').ToList();
+
+            foreach (var str in input)
+            {
+                string tmp = str.ToLower();
+                if (!strs.ContainsKey(tmp))
+                    strs.Add(tmp, 0);
+                
+                strs[tmp]++;
+            }
+
+            input.Clear();
+
+            foreach (var str in strs.Keys)
+            {
+                if (strs[str] % 2 == 1)
+                    input.Add(str);
+                
+            }
+
+            for (int i = 0; i < input.Count-1;i++)
+                Console.Write("{0}, ", input[i].ToLower());
+            
+            Console.WriteLine(input[input.Count-1].ToLower());
+        }
+    }
+}
