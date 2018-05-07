08d9e9690df4ec7850d7bcdaa9ec7c9399864e88
Georgiy Karpov <karpov21g@gmail.com>
2018-05-07 17:44:05 +0300

    Problems 1 and 2

--- Ex_3_AssocArrays/CountrealNum/Program.cs
+++ Ex_3_AssocArrays/CountrealNum/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
 
 namespace CountrealNum
 {
@@ -6,7 +10,20 @@ namespace CountrealNum
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var nums = new SortedDictionary<double, int>();
+
+            List<double> input = Console.ReadLine().Split(' ').Select(Double.Parse).ToList();
+
+            foreach(var num in input)
+            {
+                if (!nums.ContainsKey(num))
+                    nums.Add(num, 0);
+
+                nums[num]++;
+            }
+
+            foreach(var num in nums.Keys)
+                Console.WriteLine("{0} -> {1}", num, nums[num]);
         }
     }
 }
