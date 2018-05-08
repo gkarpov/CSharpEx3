a257b14b9dc06cfed7ee4b2188ad09e55280dad5
Karpov <karpo001@bg.kostal.int>
2018-05-08 16:40:35 +0300

    Added solutions 6 and 7

---
+++ Ex_3_AssocArrays_VS/MinerTask/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MinerTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dict = new Dictionary<string, long>();
+
+            int i =0;
+            string tmp = "";
+            long count;
+
+            do
+            {
+
+                i++;
+
+                if (i % 2 == 1)
+                {
+                    tmp = Console.ReadLine();
+                    if (tmp == "stop")
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        if(!dict.ContainsKey(tmp))
+                            dict[tmp] = 0;
+                    }
+                }
+                else
+                {
+                    count = long.Parse(Console.ReadLine());
+                    dict[tmp] += count;
+                }
+            } while (true);
+
+            foreach(var obj in dict)
+                Console.WriteLine("{0} -> {1}", obj.Key, obj.Value);
+
+        }
+    }
+}
