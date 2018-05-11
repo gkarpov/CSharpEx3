4786b6f1d35e00a5a78cbc4d53ce5c349c65e2f3
Georgiy Karpov <karpov21g@gmail.com>
2018-05-12 00:00:22 +0300

    finished

--- Ex_3_AssocArrays/DragonArmy/Program.cs
+++ Ex_3_AssocArrays/DragonArmy/Program.cs
@@ -9,31 +9,31 @@ namespace DragonArmy
     {
         public static void Main(string[] args)
         {
-            var dict = new SortedDictionary<string, SortedDictionary<string, Dictionary<string, long>>>();
+            var dict = new Dictionary<string, SortedDictionary<string, Dictionary<string, double>>>();
 
             int num = int.Parse(Console.ReadLine());
 
             do
             {
-                var stat = new Dictionary<string, long>();
-                var drag = new SortedDictionary<string, Dictionary<string, long>>();
+                var stat = new Dictionary<string, double>();
+                var drag = new SortedDictionary<string, Dictionary<string, double>>();
 
                 var inp = Console.ReadLine().Split(' ').ToList<string>();
 
                 if (inp[2] == "null")
                     stat["damage"] = 45;
                 else
-                    stat["damage"] = long.Parse(inp[2]);
+                    stat["damage"] = double.Parse(inp[2]);
                 
                 if (inp[3] == "null")
                     stat["health"] = 250;
                 else
-                    stat["health"] = long.Parse(inp[3]);
+                    stat["health"] = double.Parse(inp[3]);
                 
-                if (inp[3] == "null")
+                if (inp[4] == "null")
                     stat["armor"] = 10;
                 else
-                    stat["armor"] = long.Parse(inp[4]);
+                    stat["armor"] = double.Parse(inp[4]);
 
 
                 //if(dict.ContainsKey())
@@ -48,26 +48,42 @@ namespace DragonArmy
                 }
                 else
                 {
-                    dict[inp[0]] = new SortedDictionary<string, Dictionary<string, long>>();
+                    dict[inp[0]] = new SortedDictionary<string, Dictionary<string, double>>();
                     dict[inp[0]] = drag;
                 }
 
                     
                 num--;
             } while (num > 0);
+
+
+
             foreach (var dr in dict)
             {
-                Console.WriteLine("{0}: ", dr.Key);
+                Console.Write("{0}::(", dr.Key);
 
-                //var tmplist = new List<string>();
+                double dm = 0;
+                double hl = 0;
+                double am = 0;
 
-                //tmplist = usrips[usr.Key];
+                foreach(var nm in dr.Value)
+                {
+                    dm += nm.Value["damage"];    
+                    hl += nm.Value["health"];
+                    am += nm.Value["armor"];
+                }
 
-                //tmplist.Sort();
+                //Console.WriteLine("{0}/{1}/{2})", Math.Round(dm/dr.Value.Count, 2), Math.Round(hl/dr.Value.Count, 2), Math.Round(am/ dr.Value.Count, 2));
+                Console.WriteLine("{0:0.00}/{1:0.00}/{2:0.00})", dm/dr.Value.Count, hl/dr.Value.Count , am/dr.Value.Count);
 
-                //for (int i = 0; i < tmplist.Count - 1; i++)
-                //    Console.Write("{0}, ", tmplist[i]);
-                //Console.WriteLine("{0}]", tmplist
+                foreach (var nm in dr.Value)
+                {
+                    Console.WriteLine("-{0} -> damage: {1}, health: {2}, armor: {3}",
+                                      nm.Key,
+                                      nm.Value["damage"],
+                                      nm.Value["health"],
+                                      nm.Value["armor"]);
+                }
             }
         }
     }
